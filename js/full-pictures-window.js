3a6558a5b7ee3702fb84b9c82dce0bc2315c6782
MiKuul <mixailpetrograd@gmail.com>
2025-11-11 19:25:11 +0300

    Декомпозиция openPictureWindow, верное расположение обработчика closeButtonElement, добавление сокрытия commentsLoader

--- js/full-pictures-window.js
+++ js/full-pictures-window.js
@@ -5,6 +5,7 @@ const pictureWindow = document.querySelector('.big-picture');
 const closeButtonElement = pictureWindow.querySelector('.big-picture__cancel');
 const commentsLoader = pictureWindow.querySelector('.comments-loader');
 const bodyElement = document.querySelector('body');
+const commentShownCount = pictureWindow.querySelector('.social__comment-shown-count');
 
 // Временные переменные для обеспечения работоспособности функций openPictureWindow,
 // loadingComments и удаления обработчика по клику
@@ -19,10 +20,11 @@ function onDocumentKeydown (evt) {
   }
 }
 
-// Открытие и Отрисовка карточки в большом окне
-function openPictureWindow (url, description, likes, comments) {
+// Заполнение данными открывшееся окно
+function fillingPictureData (url, description, likes, comments) {
   pictureWindow.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
+  commentsLoader.classList.remove('hidden');
   document.addEventListener('keydown', onDocumentKeydown);
 
   temporaryСommentsArr = comments;
@@ -32,30 +34,46 @@ function openPictureWindow (url, description, likes, comments) {
   pictureWindow.querySelector('.social__caption').textContent = description;
   pictureWindow.querySelector('.social__comment-total-count').textContent = temporaryСommentsArr.length;
 
-  if (temporaryСommentsArr.length <= 5) {
-    renderComments(temporaryСommentsArr);
-    pictureWindow.querySelector('.social__comment-shown-count').textContent = temporaryСommentsArr.length;
-  } else {
-    const slicedArr = temporaryСommentsArr.slice(0, 5);
-    renderComments(slicedArr);
-    pictureWindow.querySelector('.social__comment-shown-count').textContent = 5;
-  }
-
-  commentsLoader.addEventListener('click', loadingComments);
+  return temporaryСommentsArr;
 }
 
-// Загрузка новых комментариев
+// Загрузка нужного числа комментариев при открытии окна
+function renderRequiredNumberOfComments (arr) {
+  if (arr.length <= 5) {
+    renderComments(arr);
+    commentShownCount.textContent = arr.length;
+    commentsLoader.classList.add('hidden');
+  } else {
+    const slicedArr = arr.slice(0, 5);
+    renderComments(slicedArr);
+    commentShownCount.textContent = 5;
+  }
+}
+
+// Загрузка новых комментариев при нажатии на "Загрузить еще"
 function loadingComments () {
   temporaryCommentsСount += 5;
   if (temporaryСommentsArr.length - temporaryCommentsСount <= 0) {
+    commentsLoader.classList.add('hidden');
     const slicedArr = temporaryСommentsArr.slice(0, temporaryСommentsArr.length);
     renderComments(slicedArr);
-    pictureWindow.querySelector('.social__comment-shown-count').textContent = temporaryСommentsArr.length;
+    commentShownCount.textContent = temporaryСommentsArr.length;
   } else {
     const slicedArr = temporaryСommentsArr.slice(0, temporaryCommentsСount);
     renderComments(slicedArr);
-    pictureWindow.querySelector('.social__comment-shown-count').textContent = temporaryCommentsСount;
+    commentShownCount.textContent = temporaryCommentsСount;
   }
+}
+
+// Открытие и отрисовка карточки в большом окне
+function openPictureWindow (url, description, likes, comments) {
+
+  fillingPictureData(url, description, likes, comments);
+
+  renderRequiredNumberOfComments(temporaryСommentsArr);
+
+  commentsLoader.addEventListener('click', loadingComments);
+  closeButtonElement.addEventListener('click', closePictureWindow);
 }
 
 // Закрытие большого окна карточки и удаление обработчиков
@@ -66,7 +84,5 @@ function closePictureWindow () {
   commentsLoader.removeEventListener('click', loadingComments);
   temporaryCommentsСount = 5;
 }
-
-closeButtonElement.addEventListener('click', closePictureWindow);
 
 export {openPictureWindow};
